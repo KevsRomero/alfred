df5314feb019dccc3ab490a3a5167ec251016140
Kevs <kevinj.romero@mercadolibre.com>
2022-08-17 00:05:56 -0300

    Update alfred-win.sh

--- alfred-win.sh
+++ alfred-win.sh
@@ -5,7 +5,7 @@ curl -o alfred_cli-windows-2.0.5.exe https://alfred-ssh.s3.amazonaws.com/bin/alf
 
 # DESCARGAR CLIENTES DE CONEXION
 curl -O https://raw.githubusercontent.com/KevsRomero/alfred/main/winssh.sh
-curl -O https://raw.githubusercontent.com/KevsRomero/alfred/main/ssh.sh
+curl -O https://raw.githubusercontent.com/KevsRomero/alfred/main/wssh.sh
 curl -O https://raw.githubusercontent.com/KevsRomero/alfred/main/instancias.txt
 
 # DAR PERMISOS DE EJECUCION
