9e06d6ee0524d31380a17e58e6f7d1465171beca
Kevs <kevinj.romero@mercadolibre.com>
2022-08-16 22:18:36 -0300

    Conexiones Alfred

---
+++ ssh.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cat instancias.txt 
+echo
+read i
+ip=(
+0 
+10.63.75.144 10.63.75.164 10.64.115.33 10.64.113.30 10.64.115.12 
+10.64.113.16 10.64.115.36 10.64.115.60 10.64.113.82 10.64.115.55 
+10.64.113.12 10.63.75.190 10.64.113.108 
+)
+eval $(ssh-agent)
+ssh-add -D
+for host in "${ip[$i]}"
+do
+echo "Conectando a $host"
+ssh -tt -A batman.adminml.com fpstech@$host
+done
