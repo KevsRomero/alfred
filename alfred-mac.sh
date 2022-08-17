1b60ba63c2d57841543825b425ec5105afe48e3a
Kevs <kevinj.romero@mercadolibre.com>
2022-08-16 23:22:39 -0300

    Update alfred-mac.sh

--- alfred-mac.sh
+++ alfred-mac.sh
@@ -4,16 +4,16 @@
 brew install git
 
 # INSTALAR CLIENTE ALFRED
-# curl alfred-ssh.s3.amazonaws.com/installers/install-latest.sh | bash
+curl alfred-ssh.s3.amazonaws.com/installers/install-latest.sh | bash
 
 # DESCARGAR CLIENTES DE CONEXION
 curl -O https://raw.githubusercontent.com/KevsRomero/alfred/main/ssh.sh
 curl -O https://raw.githubusercontent.com/KevsRomero/alfred/main/instancias.txt
 
 # DAR PERMISOS DE EJECUCION
-#chmod 777 ssh.sh
+chmod 777 ssh.sh
 
 # AGREGAR ALIAS AL TERMINAL
-#echo 'alias alfred="bash ~/ssh.sh"' >> .bash_profile
-#echo 'alias ll="ls -l"' >> .bash_profile
-#source .bash_profile
+echo 'alias alfred="bash ~/ssh.sh"' >> .bash_profile
+echo 'alias ll="ls -l"' >> .bash_profile
+source .bash_profile
