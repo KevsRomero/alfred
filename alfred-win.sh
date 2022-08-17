66e086e61435639cca256f3c1139862b85585ed1
Kevs <kevinj.romero@mercadolibre.com>
2022-08-16 23:45:46 -0300

    Update alfred-win.sh

--- alfred-win.sh
+++ alfred-win.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# INSTALAR CLIENTE ALFRED
+# DESCARGAR CLIENTE ALFRED
 curl -o alfred_cli-windows-2.0.5.exe https://alfred-ssh.s3.amazonaws.com/bin/alfred_cli-windows-2.0.5
 
 # DESCARGAR CLIENTES DE CONEXION
+curl -O https://raw.githubusercontent.com/KevsRomero/alfred/main/winssh.sh
 curl -O https://raw.githubusercontent.com/KevsRomero/alfred/main/ssh.sh
 curl -O https://raw.githubusercontent.com/KevsRomero/alfred/main/instancias.txt
 
-# DAR PERMISOS DE EJECUCION
-chmod 777 ssh.sh
+# INSTALAR CLIENTE ALFRED
 
-# AGREGAR ALIAS AL TERMINAL
-echo 'alias alfred="bash ~/ssh.sh"' >> .bash_profile
-echo 'alias ll="ls -l"' >> .bash_profile
-source .bash_profile
+./alfred_cli-windows-2.0.5.exe
+
+# DAR PERMISOS DE EJECUCION
+chmod 777 *.sh
